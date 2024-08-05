045d60eff4c5c12af8d9ccf68854aa516190b039
Stanislaw Horna <stanislawhorna@outlook.com>
2024-08-05 22:45:43 +0200

    add necessary privileges for grafana user

--- PostgreSQL/SQL/09.create_users.sql
+++ PostgreSQL/SQL/09.create_users.sql
@@ -1,3 +1,10 @@
 CREATE ROLE "grafana" LOGIN PASSWORD 'PoNyK87NB#kax';
 
-GRANT EXECUTE ON FUNCTION run_speedtest() TO "grafana";
+GRANT SELECT, INSERT, UPDATE ON Actions, Operations TO "grafana";
+GRANT SELECT, UPDATE ON SEQUENCE operations_id_seq TO "grafana";
+GRANT SELECT, UPDATE ON SEQUENCE actions_id_seq TO "grafana";
+
+GRANT EXECUTE ON PROCEDURE run_speedtest() TO "grafana";
+
+GRANT EXECUTE ON FUNCTION get_action_id(varchar) TO "grafana";
+GRANT EXECUTE ON FUNCTION get_action_completion_status(integer) TO "grafana";
