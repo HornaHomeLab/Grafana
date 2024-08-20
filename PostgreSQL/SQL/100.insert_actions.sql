5997215f54ebd2e1f0f16c2c72a62ed59b1de8c5
Stanisław Horna <stanislawhorna@outlook.com>
2024-08-20 17:05:14 +0000

    fix API port

--- PostgreSQL/SQL/100.insert_actions.sql
+++ PostgreSQL/SQL/100.insert_actions.sql
@@ -2,7 +2,7 @@ INSERT INTO Actions(Name,Endpoint_url,Method)
 VALUES('SpeedTest','http://apps.Horna.local:8000/runOnDemand', 'Post');
 
 INSERT INTO Actions(Name,Endpoint_url,Method)
-VALUES('FundQuotation','http://apps.Horna.local:5000/FundQuotation', 'Put');
+VALUES('FundQuotation','http://apps.Horna.local:5500/FundQuotation', 'Put');
 
 INSERT INTO Actions(Name,Endpoint_url,Method)
-VALUES('InvestmentRefund','http://apps.Horna.local:5000/InvestmentRefund', 'Put');
+VALUES('InvestmentRefund','http://apps.Horna.local:5500/InvestmentRefund', 'Put');
