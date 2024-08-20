776b81078cc8874c66558d2f1562b40c3b83eb9b
Stanisław Horna <stanislawhorna@outlook.com>
2024-08-20 16:07:52 +0000

    add procedures for investment project methods

--- PostgreSQL/SQL/06.create_callable_actions.sql
+++ PostgreSQL/SQL/06.create_callable_actions.sql
@@ -3,7 +3,27 @@ LANGUAGE plpgsql
 AS $$
 BEGIN
 
-    CALL run_action_post_request(get_action_id('SpeedTest'));
+    CALL run_action_request(get_action_id('SpeedTest'));
+
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE download_fund_quotation()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+
+    CALL run_action_request(get_action_id('FundQuotation'));
+
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE calculate_investment_result()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+
+    CALL run_action_request(get_action_id('InvestmentRefund'));
 
 END;
 $$
