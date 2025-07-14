015e9ae03028956c5ef8ed43f94e6bc216cc9bfd
Stanisław Horna <stanislawhorna@outlook.com>
2025-07-14 17:20:08 +0200

    use Terraform Backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,5 +9,16 @@ terraform {
 provider "grafana" {
   url                  = data.vault_generic_secret.grafana_creds.data["url"]
   auth                 = "${data.vault_generic_secret.grafana_creds.data["username"]}:${data.vault_generic_secret.grafana_creds.data["password"]}"
-  insecure_skip_verify = true 
+  insecure_skip_verify = true
+}
+terraform {
+  backend "http" {
+    address = "http://10.0.10.11:8001/state/Grafana/prod"
+  }
+}
+data "terraform_remote_state" "foo" {
+  backend = "http"
+  config = {
+    address = "http://10.0.10.11:8001/state/Grafana/prod"
+  }
 }
